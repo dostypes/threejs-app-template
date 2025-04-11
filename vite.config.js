c34db0b7b00de5eb2c30e0418c383cdf43db21e9
Unboring SL <arturo@unboring.net>
2025-04-11 13:36:26 +0200

    Update package.json and vite.config.js for deployment setup and configuration

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,27 @@ import glsl from 'vite-plugin-glsl'
 import fs from 'fs'
 import path from 'path'
 
-export default defineConfig({
-  server: {
-    open: true,
-    host: true,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './certificate.pem')),
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production'
+
+  return {
+    base: isProduction ? '/threejs-app-template/' : '/',
+    server: {
+      open: true,
+      host: true,
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, './key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, './certificate.pem')),
+      },
+      port: 8080,
     },
-    port: 8080,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  plugins: [
-    glsl({
-      include: /\.(glsl|vs|fs)$/,
-    }),
-  ],
+    build: {
+      outDir: 'dist',
+    },
+    plugins: [
+      glsl({
+        include: /\.(glsl|vs|fs)$/,
+      }),
+    ],
+  }
 })
